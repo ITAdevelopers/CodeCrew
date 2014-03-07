e94f6aa40e488bef12f18e06b7364f1161753394
mladen1978 <djamla1978@yahoo.com>
2014-03-07 01:27:03 +0100

    database.php i class_login

--- config/database.php
+++ config/database.php
@@ -5,23 +5,18 @@ class database{
 	private $host = 'localhost';
 	private $dbname = 'code_crew';
 	private $username = 'root';
-	private $password ='php';  
+	private $password ='';  
 	private $opt = array(
 	      PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
 	      PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
 	      );
-	public $con = '';
-
-
+	public $pdo = '';
 
 	public function connect(){
 
-
-        return $this->con = new PDO("mysql:host=$this->host;dbname=$this->dbname",$this->username, $this->password,$this->opt);
+      return $this->pdo = new PDO("mysql:host=$this->host;dbname=$this->dbname",$this->username, $this->password,$this->opt);
 	}
 
-	}
-
-
+}
 
 ?>
