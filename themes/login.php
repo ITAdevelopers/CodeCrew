43fbfee221ffa850ffc10eed961290fb459d2896
Nenad Paic <nenadpaic@sbb.rs>
2014-03-15 03:27:18 +0100

    login

---
+++ themes/login.php
@@ -0,0 +1,37 @@
+
+
+<!doctype html>
+<head>
+<title>Twiter</title>
+<meta charset="utf-8">
+    <title>Bootstrap, from Twitter</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+
+
+
+
+</style>
+</head>
+<body>
+    <div class="row-fluid">
+        <div class="span3" id="forma">
+    <form>
+    <fieldset>
+    <legend><h3>Login</h3></legend>
+    <label>Username:</label>
+    <input type="text" placeholder="Vas username...">
+    
+     <label>Password:</label>
+    <input type="text" placeholder="Vas password">
+    
+    
+    <button type="submit" class="btn btn-primary">Login</button>
+    </fieldset>
+    </form>
+</div>
+</div>
+</body>
+</html>
