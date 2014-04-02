3224aa658861bd988c878845dd8549effe0b502e
sucurovicd <skull_serbia@yahoo.com>
2014-04-02 23:13:45 +0200

    a ovo sam zaboravio :D

--- admin/includes/function_user.php
+++ admin/includes/function_user.php
@@ -9,22 +9,21 @@
 		}
 
 		public function listUsers(){
-			$num_of_users = $this->crud->count('users')[0][0];
 			echo "<div class='module'>";
-			echo "<h2><span>Sample table</span></h2>";
+			echo "<h2><span>List of all users</span></h2>";
             echo '<table id="myTable" class="tablesorter">';
                 echo '<thead><tr>';
                 	echo '<th width="5%">User Id</th><th width="30%">Username</th><th width="5%">Role</th><th width="15%">Last_Login</th><th width="15%">Account Created</th><th width="20%">Actions</th>';
                 echo '</tr></thead><tbody>';
-			    for ($i=1; $i<=$num_of_users; $i++){
-			         $user = $this->crud->searchUser($i)[0];
+               	$users = $this->crud->listUsers();
+               	foreach ($users as $user){
 			         echo "<tr><td>" . $user['user_id'] . "</td>";
 			         echo "<td>" . $user['username'] . "</td>";
 			         echo "<td>" . $user['role'] . "</td>";
 			         echo "<td>" . $user['last_login'] . "</td>";
 			         echo "<td>" . $user['created'] . "</td>";
-			         echo "<td><a href='users.php?action=edit&id=".$user['user_id']."'><img src='img/pencil.gif' alt='edit'>";
-			         echo "<a href='users.php?action=delete&id=".$user['user_id']."''><img src='img/minus-circle.gif' alt='delete' /></a></td></tr>";
+			         echo "<td><a href='users.php?action=edit&id=".$user['user_id']."'><img src='img/pencil.gif' alt='edit' title='Edit User'>";
+			         echo "<a href='users.php?action=delete&id=".$user['user_id']."''><img src='img/minus-circle.gif' alt='delete' title='Delete User'></a></td></tr>";
 			    }
 			echo "</tbody></table></div>";
 			}
@@ -72,23 +71,34 @@
 			}
 			$username = (isset($username)) ? $username : $user['username'];
 			$role = (isset($role))? $role : $user['role'];
-			echo "<form method='post' action='".$_SERVER['PHP_SELF']."?action=edit&id=".$id."'>";
-				echo "<input type='text' name='id' value='".$user['user_id']."'></input><br>";
-				echo "<input type='text' name='username' value='$username'></input><br>";
-				echo "<input type='text' name='password' value=''></input><br>";
-				echo "<input type='text' name='password2' value=''></input><br>";
-				echo "<select name='role'>";
-					for ($i=1; $i<=$num_of_roles; $i++){
-						$selected = "";
-						$role1 = $this->crud->searchRole($i)[0];
-						if ($role1['role'] == $role)
-							$selected = "selected='selected'";
-						echo "<option value='".$role1['role_id']."'$selected>".$role1['role']."</option>";
-					}
-				echo "</select><br>";
-				echo "<input type='submit' name='submit' value='Submit'>";
-				echo "<input type='reset' name='Reset Data'>";
-			echo "</form>";	
+			echo "<div class='module'>";
+				echo "<h2><span>Form</span></h2>";         
+	            echo '<div class="module-body">';
+					echo "<form method='post' action='".$_SERVER['PHP_SELF']."?action=edit&id=".$id."'>";
+						echo "<label for='id'>User id:</label>";
+						echo "<input type='text' name='id' value='".$user['user_id']."'></input><br>";
+						echo "<label for'username'>Username: </label>";
+						echo "<input type='text' name='username' value='$username'></input><br>";
+						echo "<label for='password'>New password</label>";
+						echo "<input type='password' name='password' value=''><br>";
+						echo "<label for='rpassword2'>Retype password</label>";
+						echo "<input type='password' name='password2' value=''></input><br>";
+						echo "<label for='role'>Role</label>";
+						echo "<select name='role'>";
+							for ($i=1; $i<=$num_of_roles; $i++){
+								$selected = "";
+								$role1 = $this->crud->searchRole($i)[0];
+								if ($role1['role'] == $role)
+									$selected = "selected='selected'";
+								echo "<option value='".$role1['role_id']."'$selected>".$role1['role']."</option>";
+							}
+						echo "</select><br>";
+						echo "<input type='submit' name='submit' value='Submit' class='submit-green'>";
+						echo "<input type='reset' name='Reset Data' class='submit-gray'>";
+					echo "</form>";	
+				echo "</div>";
+			echo "</div>";
+
 		}
 
 		public function createUser(){
@@ -103,11 +113,15 @@
 						if ($this->val->isPasswordValid($password)){
 							$this->crud->addUser($username, $password, $role);
 							$user = $this->crud->searchUser($username)[0];
-							echo "You have added user with this data:<br>";
-							echo "id: ".$user['user_id']."<br>";
-							echo "username: ".$user['username']."<br>";
-							echo "Role: ".$user['role']."<br>";
-							echo "Date Created: ".$user['created'];
+							echo '<span class="notification n-success">Successfuly added user</span>';
+							echo "<div class='module'>";
+								echo "<h2><span>You have added user with this data:</span></h2>";
+								echo '<div class="module-body">';
+								echo "<strong>Id:</strong> ".$user['user_id']."<br>";
+								echo "<strong>Username:</strong> ".$user['username']."<br>";
+								echo "<strong>Role:</strong> ".$user['role']."<br>";
+								echo "<strong>Date Created:</strong> ".$user['created'] . "<br>";
+								echo "</div></div>";
 							exit;
 						}
 						else{
@@ -126,9 +140,9 @@
 				echo "<label for'username'>Username:</label>";
 				echo "<input type='text' name='username' value='$username'><br>";
 				echo "<label for='password'>Password</label>";
-				echo "<input type='text' name='password'><br>";
+				echo "<input type='password' name='password'><br>";
 				echo "<label for='password1'>Retype Password</label>";
-				echo "<input type='text' name='password1'><br>";
+				echo "<input type='password' name='password1'><br>";
 				echo "<label for='role'>Select Role</label>";
 				echo "<select name='role'>";
 					for ($i=1; $i<=$num_of_roles; $i++){
@@ -139,7 +153,7 @@
 						echo "<option value='".$role1['role_id']."'$selected>".$role1['role']."</option>";
 					}
 				echo "</select><br>";
-				echo "<input type='submit' name='register' value='Create'>";
+				echo "<input type='submit' name='register' value='Create' class='submit-green'>";
 			echo "</form>";
 
 
@@ -150,21 +164,24 @@
 					$id = $this->search();
 					}
 			$user = $this->crud->searchUser($id)[0];
+			echo "<div class='module'>";
 			if (isset($_POST['delete'])){
 				$this->crud->deleteUser($id);
-
+				echo "<h2><span>You have deleted this user:</span></h2><br>";
 			}
-
-			echo "Are you sure you want to delete this user? :<br>";
-			echo "id: ".$user['user_id']."<br>";
-			echo "username: ".$user['username']."<br>";
-			echo "Role: ".$user['role']."<br>";
-			echo "Date Created: ".$user['created'] . "<br>";
+			else {
+				echo "<h2><span>Are you sure you want to delete this user?:</h2></span><br>";
+			}
+			echo '<div class="module-body">';
+			echo "<strong>Id:</strong> ".$user['user_id']."<br>";
+			echo "<strong>Username:</strong> ".$user['username']."<br>";
+			echo "<strong>Role:</strong> ".$user['role']."<br>";
+			echo "<strong>Last Login:</strong>".$user['last_login']."<br>";
+			echo "<strong>Date Created:</strong> ".$user['created'] . "<br>";
 			echo "<form method='post' action='".$_SERVER['PHP_SELF']."?action=delete&id=$id'>";
-			echo "<input type='submit' name='delete' value='Delete'>";
+			echo "<input type='submit' name='delete' value='Delete' class='submit-green'>";
 			echo "</form>";
-
-
+			echo '</div></div>';
 
 		}
 
@@ -175,21 +192,16 @@
 				if (!empty($user))
 					return $user[0]['user_id'];
 				else
-					echo "User ne postoji";
-
+					echo '<span class="notification n-error">Korisnik sa tim nadimkom ne postoji</span>';
 			}
 			$action = $_GET['action'];
 			echo "<form method='post' action='".$_SERVER['PHP_SELF']."?action=".$action."'>";
 			echo "<input type='text' name='search_username'>";
-			echo "<input type='submit' name='search' value='Search'>";
+			echo "<input type='submit' name='search' value='Search' class='submit-green'>";
 			echo "</form>";
 			exit;
 				
 		}
-			
-		
-
-
 	}
 
 ?>
